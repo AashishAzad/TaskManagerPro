0a983dbc440e217ca4389201e1e5336dcc888074
aashish_tmpro <aashishazad9@gmail.com>
2026-02-10 02:36:25 +0530

    Single Task Deletion Fix.

--- lib/features/tasks/widgets/task_actions_sheet.dart
+++ lib/features/tasks/widgets/task_actions_sheet.dart
@@ -120,20 +120,16 @@ class TaskActionsSheet extends StatelessWidget {
               );
             },
           ),
-
-          // Currently, user can only delete the task using "Clear All" option.
-          // const Divider(height: 1, color: AppColors.borderDark),
-          //
-          // _buildActionTile(
-          //   context,
-          //   icon: Icons.delete,
-          //   title: 'Delete Task',
-          //   color: AppColors.error,
-          //   onTap: () {
-          //     Navigator.pop(context);
-          //     _showDeleteConfirmation(context);
-          //   },
-          // ),
+          const Divider(height: 1, color: AppColors.borderDark),
+          _buildActionTile(
+            context,
+            icon: Icons.delete,
+            title: 'Delete Task',
+            color: AppColors.error,
+            onTap: () {
+              _showDeleteConfirmation(context);
+            },
+          ),
         ],
       ),
     );
